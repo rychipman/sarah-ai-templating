920eb30b1f055ae6c618ce3317df991bc5bb67a0
Ryan Chipman <ryan@ryanchipman.com>
2020-04-01 10:49:04 -0400

    scripting improvements

--- template.sh
+++ template.sh
@@ -7,11 +7,16 @@ outdir=output
 
 rm -rf "$outdir"
 mkdir -p "$outdir"
+mkdir -p "$outdir/ai"
+mkdir -p "$outdir/zip"
 
 cat "$infile" \
     | sed -e 's/\r/\n/g' \
     | sed -e '/^,/d' \
     | sed -e '/^$/d' \
     | sed -e 's/,/ /' \
+    | sed -e "s/'//" \
     | sed -e 's/\//_/' \
-    | xargs -i'{}' cp template.ai output/{}.ai
+    | xargs -i'{}' cp template.ai "$outdir/ai/{}.ai"
+
+zip "$outdir/zip/styles.zip" "$outdir"/ai/*.ai
